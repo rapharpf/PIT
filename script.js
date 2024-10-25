9a76b3859c7145cd43cd20782292d937c238c84b
Raphael <rapha.p.f@hotmail.com>
2024-10-25 17:07:12 -0400

    sidenav

---
+++ script.js
@@ -0,0 +1,18 @@
+/*let teste = document.getElementById("teste");
+let teste2 = document.querySelectorAll("#teste");
+teste.style.width = "100px";
+console.log(teste);
+console.log(teste2);
+*/
+
+/*Alterando a largura do menu lateral para 250px*/ 
+function openNav() {
+    let teste1 = document.getElementById("MySidenav")
+    console.log(teste1);
+    document.getElementById("MySidenav").style = width ="250px";
+}
+
+/* Alterando a largura do menu lateral para 0p*/
+function closeNav() {
+    document.getElementById("Mysidenav").style.width = "0px";
+} 
