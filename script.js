ec0abb8b3e66c1d0d78134135a4ba32d67ddd0ff
Raphael <rapha.p.f@hotmail.com>
2024-10-26 12:12:56 -0400

    pag cadastrar

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-/*Alterando a largura do menu lateral para 250px*/
+/*Alterando a largura do menu lateral para 280px*/
 function openNav() {
     document.getElementById("mySidenav").style.width = "280px";
     document.getElementById("main").style.marginLeft = "280px";
     document.getElementById("openBtn").style.display = "none";
     document.getElementById("closeBtn").style.display = "initial";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.body.style.backgroundColor = "rgba(32,32,32)";
 }
 
-/* Alterando a largura do menu lateral para 0px*/
+/* Alterando a largura do menu lateral para 30px*/
 function closeNav() {
     document.getElementById("mySidenav").style.width = "30px";
     document.getElementById("main").style.marginLeft = "30px";
     document.getElementById("openBtn").style.display = "initial";
     document.getElementById("closeBtn").style.display = "none";
-    document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "rgba(40,40,40)";
 }
