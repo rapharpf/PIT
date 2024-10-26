b2d5d62144200ac475acf4564e4fd4fc9f58bced
Raphael <rapha.p.f@hotmail.com>
2024-10-26 11:20:55 -0400

    side nav ok

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-/*let teste = document.getElementById("teste");
-let teste2 = document.querySelectorAll("#teste");
-teste.style.width = "100px";
-console.log(teste);
-console.log(teste2);
-*/
-
-/*Alterando a largura do menu lateral para 250px*/ 
+/*Alterando a largura do menu lateral para 250px*/
 function openNav() {
-    let teste1 = document.getElementById("MySidenav")
-    console.log(teste1);
-    document.getElementById("MySidenav").style = width ="250px";
+    document.getElementById("mySidenav").style.width = "280px";
+    document.getElementById("main").style.marginLeft = "280px";
+    document.getElementById("openBtn").style.display = "none";
+    document.getElementById("closeBtn").style.display = "initial";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
-/* Alterando a largura do menu lateral para 0p*/
+/* Alterando a largura do menu lateral para 0px*/
 function closeNav() {
-    document.getElementById("Mysidenav").style.width = "0px";
-} 
+    document.getElementById("mySidenav").style.width = "30px";
+    document.getElementById("main").style.marginLeft = "30px";
+    document.getElementById("openBtn").style.display = "initial";
+    document.getElementById("closeBtn").style.display = "none";
+    document.body.style.backgroundColor = "white";
+}
